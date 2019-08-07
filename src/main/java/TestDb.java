06f97845e385414d112d1c44bcd8d6afc179fc5a
marco <marco@marco-desktop.fritz.box>
2019-08-07 13:50:38 +0200

    Configure JPA and added first Entity

---
+++ src/main/java/TestDb.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestDb {
+	public static void main(String a[]) {
+		String username = "root";
+		String password = "password";
+		String connString = "jdbc:mysql://" + "localhost" + ":" + "3306" + "/"
+				+ "fitnesstracker";
+		String JDBCDriver = "com.mysql.jdbc.Driver";
+
+		try {
+			// Try and load the JDBC driver
+			Class.forName(JDBCDriver);
+
+			// Try and actually connect
+			Connection con = DriverManager.getConnection(connString, username,
+					password);
+			System.out.println("Connected Successfully");
+		} catch (SQLException ex) {
+			System.out.println(ex);
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Driver not found: " + ex);
+		}
+	}
+} // end
